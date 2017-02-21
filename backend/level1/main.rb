3088f3e62485a20d65a5f998074eec35aa774a2e
Mochamad Gufron <mgufronefendi@gmail.com>
2017-02-21 15:04:22 +0700

    ruby code

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,3 +1,15 @@
 require "json"
-
+require "date"
 # your code
+data = JSON.parse File.read("data.json")
+cars = data["cars"]
+rentals = [];
+data["rentals"].map do |e|
+  car = cars.find { |car| car["id"] == e["car_id"] }
+  rentals << {
+    id: e["id"],
+    price: (e["distance"] * car["price_per_km"]) + (((Date.parse(e["start_date"])...Date.parse(e["end_date"])).count + 1) * car["price_per_day"])
+  }
+end
+
+open("output.json", "w"){|file| file.write(JSON.pretty_generate({rentals: rentals}))}
