24184cd20d8288d8d96ae1f890824937a1e39f47
Mochamad Gufron <mgufronefendi@gmail.com>
2017-02-21 19:40:24 +0700

    update main code

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,6 +1,16 @@
 require "json"
 require "date"
 # your code
+def price_km (per_km, distance)
+  per_km * distance
+end
+def get_days(start_date, end_date)
+  (Date.parse(start_date)...Date.parse(end_date)).count + 1
+end
+def price_day(per_day, days)
+  per_day * days
+end
+
 data = JSON.parse File.read("data.json")
 cars = data["cars"]
 rentals = [];
@@ -8,7 +18,7 @@ data["rentals"].map do |e|
   car = cars.find { |car| car["id"] == e["car_id"] }
   rentals << {
     id: e["id"],
-    price: (e["distance"] * car["price_per_km"]) + (((Date.parse(e["start_date"])...Date.parse(e["end_date"])).count + 1) * car["price_per_day"])
+    price: price_km(car["price_per_km"], e["distance"]) + price_day(car["price_per_day"], get_days(e["start_date"], e["end_date"]))
   }
 end
 
